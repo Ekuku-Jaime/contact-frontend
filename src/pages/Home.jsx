cd193d79e711e4c60d48fc76f23d3ec9ac1f09cf
domingos <domingoscavele01@gmail.com>
2022-05-15 21:33:13 +0200

    feat: added the get contacts funcionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
 import styled from "@emotion/styled";
 import { Container, Typography } from "@mui/material";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Contact from "../components/Contact";
 import ContactForm from "../components/ContactForm";
 import { data } from "../data";
 import { ContactContextProvider } from "../state/context";
 
 export default function Home() {
+  const [contacts, setContacts] = useState();
+
+  const getContacts = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/v1/contacts/"
+      );
+      setContacts(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getContacts();
+  }, []);
+
   return (
     <ContactContextProvider>
       <Container>
@@ -15,9 +32,10 @@ export default function Home() {
           <Typography variant="h2" sx={{ mt: 2, mb: 2 }}>
             Your Contacts
           </Typography>
-          {data.map((contact) => (
-            <Contact contacto={contact} key={contact.number} />
-          ))}
+          {contacts &&
+            contacts.map((contact) => (
+              <Contact contacto={contact} key={contact.number} />
+            ))}
         </ContentStyle>
       </Container>
     </ContactContextProvider>
